c855c79fb6fd0f8fc57b57d0df28950d83d67866
Marco A. Harrendorf <marco.harrendorf@cern.ch>
2016-05-18 15:22:30 +0200

    Basecar: Fixed some bugs

--- src/basecar.py
+++ src/basecar.py
@@ -9,18 +9,18 @@
 # Start functions and variables with lowercase
 # Member variables have a single underscore in front
 
-class BaseCar(object):
+class BaseCar:
     """ Base class for cars"""
     def __init__(self,x=0,velocity=0,brakeDistance=-1,acceleration=0,maxAcceleration=0,maxDeceleration=0,maxSpeed=0,):
         self._x = x
         self._velocity=velocity
-        self._acceleration=acceleration
-        self._maxAcceleration = acceleration
-        self._maxDeceleration = deceleration
+        self._acceleration=maxAcceleration
+        self._maxAcceleration = maxAcceleration
+        self._maxDeceleration = maxDeceleration
         self._maxSpeed = maxSpeed
         self._brakeDistance = float(-1)
-        self._previous = int()
-        self._nextNeighbour = BaseCar()
+        self._previous = Null
+        self._nextNeighbour = Null
         self._lane = int(1)
         self._neighbourX=float()
         self._neighbourV=float()
