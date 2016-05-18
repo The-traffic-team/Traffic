39419b24a574b247a9c1ee9d87ec7b7a74d8f36e
Nabarun21 <ndev@nd.edu>
2016-05-18 16:55:25 +0200

    adding more functionality

--- src/simplecar.py
+++ src/simplecar.py
@@ -4,18 +4,21 @@ from basecar import BaseCar
 class SimpleCar(BaseCar):
     """ A simple car class, inherits from BaseCar"""
     
-    def __init__(self,x=0,velocity=1,brakeDistance=-1,acceleration=0,maxAcceleration=0,maxDeceleration=0,maxSpeed=0): 
+    def __init__(self,x=0,velocity=1,brakeDistance=-1,acceleration=0,maxAcceleration=0,maxDeceleration=0,maxSpeed=200): 
         self._x=x  
         self._velocity=velocity
         self._acceleration=acceleration
         self._brakeDistance = brakeDistance
-        self._driverMax=np.random
+        self._maxSpeed=maxSpeed
+        self._driverMax=np.random.choice(range(self._maxSpeed/2,self._maxSpeed-10))
+        
+
     def updatePosition(self,time):
         self._x+=self._velocity*time
         self._velocity+=self._acceleration*time
         if(0):
             if ((self.neighbourX()-self._x)>self._brakeDistance):
-                if((self._velocity+self._acceleration*time)<=self._maxSpeed):
+                if((self._velocity+self._acceleration*time)<=self._driverMax):
                     self._velocity+=self._acceleration*time
                     self._x+=self._velocity*time
             else:    
