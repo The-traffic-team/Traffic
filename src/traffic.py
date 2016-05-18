ce79ce34a039ee8f28873d23cfaa35d4bee3d2b2
sctkjrc <kcormier@physics.utoronto.ca>
2016-05-18 14:17:08 +0000

    updated traffic.py to a first pass at basic functionality, updated first pass at plottraffic.py to allow updating (and moved to src), updated singleton to conform to style standards

--- src/traffic.py
+++ src/traffic.py
@@ -1,16 +1,40 @@
+import argparse
 
-def initialize():
-    print "traffic.py: initializing traffic simulation"    
+@Singleton
+class TrafficManager:
+    """TrafficManager class is in charge of intializing and implementing the traffic simulation"""
+    def __init__(self)
+        print "TrafficManager(): initializing traffic simulation"    
+        self.cars = [SimpleCar(0), SimpleCar(20), SimpleCar(40), SimpleCar(80)]                 
+        self.roadLength = 50
+        self.initCars()
 
-def run():
-    print "traffic.py: starting traffic simulation"
+    def initCars():
+        for i in range(len(self.cars) - 1):
+            self.cars[i].setNeighbour(self.cars[i+1])
+        self.cars[-1].setNeighbour(self.cars[0])
+    
+    def updateCars():
+        for car in self.cars:
+            car.saveState()
+        
+        for car in self.cars:
+            car.update(1)        
 
-def finalize():
-    print "traffic.py: finalizing traffic simulation"
+    def finalize():
+        print "TrafficManager(): finalizing traffic simulation"
 
 
 if __name__ == '__main__':
-    initialize()
-    run()
-    finalize()
-    
+
+    ` 
+    trafficControl = TrafficManager().instance()
+    initPlot(trafficControl)
+ 
+    for step in range(10000):
+        trafficControl.updateCars()
+        plot(trafficControl.cars) 
+        log(trafficControl.cars)    
+
+    trafficControl.finalize()
+        
