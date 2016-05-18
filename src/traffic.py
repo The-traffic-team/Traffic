2ee01d2b8fc7f0fb5d94e5f38b419fa821399766
sctkjrc <udrt@localhost.localdomain>
2016-05-18 12:59:17 +0000

    adding mostly empty traffic.py as main program

---
+++ src/traffic.py
@@ -0,0 +1,16 @@
+
+def initialize():
+    print "traffic.py: initializing traffic simulation"    
+
+def run():
+    print "traffic.py: starting traffic simulation"
+
+def finalize():
+    print "traffic.py: finalizing traffic simulation"
+
+
+if __name__ == '__main__':
+    initialize()
+    run()
+    finalize()
+    
