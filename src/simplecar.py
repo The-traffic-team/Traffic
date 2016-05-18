650266f597e500c5529ee549e207f1579da5f448
Nabarun21 <ndev@nd.edu>
2016-05-18 15:15:47 +0200

    update simplecar,basecar

--- src/simplecar.py
+++ src/simplecar.py
@@ -2,20 +2,20 @@ import basecar
 
 class SimpleCar(BaseCar):
     """ A simple car class, inherits from BaseCar"""
-
+    
     def __init__(self,x=0,velocity=0,brakeDistance=-1,acceleration=0,maxAcceleration=0,maxDeceleration=0,maxSpeed=0): 
         self._x=x  
         self._velocity=velocity
         self._brakeDistance = brakeDistance
         
-    def updatePosition(self):
-        self._x+=self._velocity*TIMESTEP
+    def updatePosition(self,time):
+        self._x+=self._velocity*time
         if(0):
             if ((self.neighbourX()-self._x)>self._brakeDistance):
-                if((self._velocity+self._acceleration*TIMESTEP)<=self._maxSpeed):
-                    self._velocity+=self._acceleration*TIMESTEP
-                    self._x+=self._velocity*TIMESTEP
+                if((self._velocity+self._acceleration*time)<=self._maxSpeed):
+                    self._velocity+=self._acceleration*time
+                    self._x+=self._velocity*time
             else:    
-                self._velocity-=self._acceleration*TIMESTEP
-                self._x+=self._velocity*TIMESTEP
+                self._velocity-=self._acceleration*time
+                self._x+=self._velocity*time
            
