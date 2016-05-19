a0175f5c244c5e2118df8fde01e43a0edb2b5401
Marc <m.scott@soton.ac.uk>
2016-05-19 16:06:16 +0100

    last collision update,hopefully

--- src/simplecar.py
+++ src/simplecar.py
@@ -4,17 +4,17 @@ from basecar import BaseCar
 class SimpleCar(BaseCar):
     """ A simple car class, inherits from BaseCar"""
     
-    def __init__(self,x=0,velocity=1,brakeDistance=10,acceleration=15,maxAcceleration=0,maxDeceleration=0,maxSpeed=200,collide=True): 
+    def __init__(self,x=0,velocity=1,brakeDistance=10,acceleration=20,maxAcceleration=0,maxDeceleration=0,maxSpeed=200,collide=True): 
 	BaseCar.__init__(self)
         self._x=x  
         self._velocity=velocity
-        self._acceleration=acceleration
         self._maxSpeed=maxSpeed
         self._driverMax=np.random.choice(range(self._maxSpeed/5,self._maxSpeed-10))
-        self._driverMood = 0.01*np.random.choice(range(40,120,5))        
+        self._driverMood = 0.01*np.random.choice(range(60,120,5))        
         self._color=np.random.choice('r,g,b,c,m,y,k'.split(','))
         self._delay=0
         self._collide=collide
+        self._acceleration=acceleration/self._driverMood
 #        print self._driverMood
     def updatePosition(self,time):
         self._brakeDistance = self._driverMood*self._velocity*self._velocity/(2* self._acceleration)        #get current minimum breaking distance
@@ -45,11 +45,12 @@ class SimpleCar(BaseCar):
                     self._x=self._x%self.ROADLENGTH
                     
     def collision(self,tempDist,time):       
-        if (tempDist+self._velocity*time<30):
+        if (tempDist+self._velocity*time<36):
             tempVel=self._neighbourV
             self.getNextNeighbour().setVelocity(self._velocity)
-            self.getNextNeighbour().setPosition(self._neighbourX+30)
-            self._velocity=tempVel/5
-            self._x=self._x-10
-            self._delay=10
-            self.getNextNeighbour().setDelay(3)
+            self.getNextNeighbour().setPosition(min(self._neighbourX+20,self.getNextNeighbour().getNextNeighbour().getPosition()))
+            self._velocity=tempVel/2
+            self._acceleration=self._acceleration/2
+            self._x=self._x
+            self._delay=6
+            self.getNextNeighbour().setDelay(1)
